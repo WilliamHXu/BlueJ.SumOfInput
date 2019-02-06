026c47b794df2e5c312634fd30f533640721cca5
WilliamXu <williamxu@zipcoders-MacBook-Pro-90.local>
2019-02-06 10:27:52 -0500

    Finished Lab

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
+
 public class Main {
 
+    
     public static void main(String[] args){
-
+        Scanner scanner = new Scanner(System.in);
+        int answer = 0;
+        System.out.println("Please Enter A Number to Sum To: ");
+        int input = scanner.nextInt();
+        for (int i = 0; i <= input; i++) {
+            answer += i;
+        }
+        System.out.println(answer);
     }
 }
